c20c1734ceff84b40257a658902ed78e8539637e
Gabriel Medeiros Patrocinio <62565345+gabs73@users.noreply.github.com>
2026-05-27 00:26:21 +0000

    Inclusão das variáveis das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,10 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char estado_1, estado_2, cod_1, cod_2;
+  char nome_1[30], nome_2[30];
+  int populacao_1, populacao_2,turismo_1, turismo_2;
+  float area_1, area_2, pib_1, pib_2;
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
